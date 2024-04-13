0324708086091802e8d400ea9a0e2bf33f3aae0c
Vinicius Petratti <89108219+ViniPetra@users.noreply.github.com>
2024-04-12 21:07:00 -0300

    Limpeza de coisa do replit

--- .ccls-cache/@home@runner@VM-Neander/main.c
+++
@@ -1,7 +0,0 @@
-#include "Codes/Assembler.h"
-#include "Codes/Lexer.h"
-#include "Codes/NeanderVM.h"
-#include "Codes/Parser.h"
-#include <stdio.h>
-
-int main(int argc, char *argv[]) { ExecutarParser(); }
